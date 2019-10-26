039545284de9e19d97f95165fee7981ac18bc821
BradleyTim <timbradley94@gmail.com>
2019-10-26 08:02:09 +0300

    connect to MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.json({ msg: "HELLO WORD" });
+const recipes = require("./routes/recipes");
+
+mongoose
+  .connect(
+    'mongodb://recipe:recipe1234@ds139869.mlab.com:39869/meowers',
+  { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log("Mongo connected successfully");
+  })
+  .catch(error => {
+    console.log("There was an error");
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/api/recipes", recipes);
+
+app.use("/", (req, res, next) => {
+  res.status(200).send('<h1 style="text-align: center">RECIPE API</h1>');
+  next();
 });
 
 module.exports = app;
---
+++ routes/recipes.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+
+const Recipe = require("../models/recipe");
+
+router.get("/", (req, res, next) => {
+  Recipe.find()
+    .then(recipes => {
+      res.status(200).json(recipes);
+    })
+    .catch(error => {
+      res.status(400).json({ error });
+    });
+});
+
+router.post("/", (req, res, next) => {
+  const recipe = new Recipe({
+    name: "chapo recipe",
+    imageUrl: "",
+    ingredients: "wheat floor, hot water, salt, cooking oil, sugar",
+    description: "whatever"
+  });
+
+  recipe
+    .save()
+    .then(() => {
+      res.status(201).json({ msg: "CREATED SUCESSFULLY" });
+    })
+    .catch(error => {
+      res.status(400).json({
+        error: error
+      });
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  Recipe.findOne({ _id: req.params.id })
+    .then(recipe => {
+      res.status(200).json(recipe);
+    })
+    .catch(error => {
+      res.status(400).json({ error });
+    });
+});
+
+router.put("/:id", (req, res, next) => {
+  const recipeUpdate = new Recipe({
+    _id: req.params.id,
+    name: req.body.name,
+    imageUrl: req.body.imageUrl,
+    ingredients: req.body.ingredients,
+    description: req.body.description
+  });
+
+  Recipe.updateOne({ _id: req.params.id }, recipeUpdate)
+    .then(() => {
+      res.status(201).json({ msg: "RECIPE UPDATED SUCCESSFULLY" });
+    })
+    .catch(error => {
+      res.status(400).json({ error });
+    });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Recipe.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({ msg: "RECIPE DELETED" });
+    })
+    .catch(error => {
+      res.status(400).json({ error });
+    });
+});
+
+module.exports = router;
